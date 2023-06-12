2309ec48148c1db9008b6142888ed50a289b9c2b
Ahilesov <contextus.lab@ya.ru>
2023-06-12 14:43:41 +0500

    Init Commit 2.2

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        buttonSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        buttonMedia.setOnClickListener {
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/MediaActivity.kt
@@ -0,0 +1,19 @@
+package com.practicum.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media)
+
+        val back = findViewById<ImageView>(R.id.back)
+
+        back.setOnClickListener {
+            finish()
+        }
+
+    }
+}
