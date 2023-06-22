63f4d4d6a1c72b4901f5aa7905ea56c30d433d2e
Ahilesov <contextus.lab@ya.ru>
2023-06-22 10:12:23 +0500

    Init Commit 2.4 styles

--- app/src/main/java/com/practicum/playlistmaker/MediaActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MediaActivity.kt
@@ -1,13 +1,18 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class MediaActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
+
+
 
         val back = findViewById<ImageView>(R.id.back)
 
