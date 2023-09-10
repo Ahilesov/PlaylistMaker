6a570e83f3cc35241ad4afb7e1744452c060bb33
Ahilesov <contextus.lab@ya.ru>
2023-09-10 20:18:26 +0500

    Init Commit 1.0 14-sprint

--- app/src/main/java/com/practicum/playlistmaker/Constants.kt
+++ app/src/main/java/com/practicum/playlistmaker/Constants.kt
@@ -6,4 +6,11 @@ object Constants {
     const val ITUNES_BASE_URL = "https://itunes.apple.com"
     const val HISTORY_TRACK_FILE = "history_track_file"
     const val HISTORY_TRACK_KEY = "key_for_history_track"
+    const val SEARCH_DEBOUNCE_DELAY = 2000L
+    const val CLICK_DEBOUNCE_DELAY = 1000L
+    const val TRACK_TIMER_DEBOUNCE_DELAY = 500L
+    const val STATE_DEFAULT_PLAYER = 0
+    const val STATE_PREPARED_PLAYER = 1
+    const val STATE_PLAYING_PLAYER = 2
+    const val STATE_PAUSED_PLAYER = 3
 }
